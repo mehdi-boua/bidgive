3d50d9193de7dc5e2827964225ccdd79b1c6aa86
mehdi bouazabia <mbouazabia@gmail.com>
2022-12-06 00:33:12 +0100

    front removed

--- src/main/java/fr/bidgive/api/controller/CategorieController.java
+++ src/main/java/fr/bidgive/api/controller/CategorieController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cat")
+@RequestMapping("/cat")
 public class CategorieController {
     @Autowired
     CategorieService categorieService;
