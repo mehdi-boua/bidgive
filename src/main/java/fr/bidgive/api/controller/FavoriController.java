c758d3ca86f8e69104294b01c0939134d2745c82
Mehdi Bouazabia <mehdi.bouazabia@univ-lyon2.fr>
2022-12-07 09:42:39 +0100

    created FavoriController

---
+++ src/main/java/fr/bidgive/api/controller/FavoriController.java
@@ -0,0 +1,25 @@
+package fr.bidgive.api.controller;
+
+
+import fr.bidgive.api.model.Favori;
+import fr.bidgive.api.service.FavoriService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/fav")
+public class FavoriController {
+
+    @Autowired
+    FavoriService favoriService;
+
+    @GetMapping("/{idUser}")
+    public Iterable<Favori> getFavs (@PathVariable final int idUser){
+        return favoriService.getAll(idUser);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteFav(@RequestBody Favori favori){
+        favoriService.deleteFavori(favori);
+    }
+}
