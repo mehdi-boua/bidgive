728083f7459e2f5e7e2225ab466651becddf7bc6
mehdi bouazabia <mbouazabia@gmail.com>
2022-12-08 00:58:50 +0100

    added atribute options for catdesc

--- src/main/java/fr/bidgive/api/controller/returnBeans/CategorieDescReturn.java
+++ src/main/java/fr/bidgive/api/controller/returnBeans/CategorieDescReturn.java
@@ -6,14 +6,13 @@ public class CategorieDescReturn {
     private int id;
     private String titre;
     private String description;
-
     private boolean options;
 
     public CategorieDescReturn(CategorieDescription categorieDescription){
         this.id = categorieDescription.getId();
         this.titre = categorieDescription.getTitre();
         this.description = categorieDescription.getDescription();
-        this.options = categorieDescription.getOptions();
+        this.options = categorieDescription.isOptions();
     }
 
     public int getId() {
