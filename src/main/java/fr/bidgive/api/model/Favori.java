67e73c6f95233e3c38479eea9b3e62bfb2347e9b
mehdi bouazabia <mbouazabia@gmail.com>
2022-12-15 17:56:20 +0100

    bugfix: Favorite delete working

--- src/main/java/fr/bidgive/api/model/Favori.java
+++ src/main/java/fr/bidgive/api/model/Favori.java
@@ -4,18 +4,19 @@ import lombok.Data;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name="favoris")
 public class Favori {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @org.springframework.data.annotation.Id
+    private int id;
+
     private @Column("idUser") int idUser;
 
     private @Column("idProduit") int idProduit;
+
 }
--- src/main/java/fr/bidgive/api/repository/FavoriRepo.java
+++ src/main/java/fr/bidgive/api/repository/FavoriRepo.java
@@ -1,6 +1,7 @@
 package fr.bidgive.api.repository;
 
 import fr.bidgive.api.model.Favori;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ public interface FavoriRepo extends CrudRepository<Favori, Integer> {
     @Query("SELECT * FROM favoris WHERE idUser = :idUser")
     Iterable<Favori> getUserFavoris(final int idUser);
 
-    @Query("DELETE FROM `favoris` WHERE `idUser`= :idUser and `idProduit` :idProduit ")
-    void deleteFavori(final int idProduit, final int idUser);
+    @Modifying
+    @Query("DELETE FROM `favoris` WHERE `idUser`= :idUser and `idProduit` = :idProduit ")
+    void deleteFavori(final int idUser, final int idProduit);
 }
